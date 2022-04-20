3045b23f6662e765eea2bfff5e46c10bf5734cf2
userlg <guipeluis90@gmail.com>
2022-04-20 11:43:01 -0400

    Line comment in database.ts

--- database.ts
+++ database.ts
@@ -2,8 +2,12 @@ import {connect } from 'mongoose';
 
 export const startConnection = async () => {
     try {
-        const db = await connect('mongodb://mongo/taskdb');
-        console.log("Connected-->" + db.connection.name);
+        //Comment this line to work locally
+        const db = await connect('mongodb://localhost/taskdb');
+        //Decomment this line to work with containers
+        //const db = await connect('mongodb://mongo/taskdb');
+
+        console.log("\t Connected-->" + db.connection.name);
     }
     catch (err) {
         console.error(err);
